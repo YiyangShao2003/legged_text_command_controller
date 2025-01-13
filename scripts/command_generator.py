4ffb404b649b1455d749fb410ae234f7134a0f8a
Kevin Shao <shaoyy@berkeley.edu>
2025-01-12 22:41:58 -0800

    mark commands to test

--- scripts/command_generator.py
+++ scripts/command_generator.py
@@ -7,11 +7,11 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 clip_model, preprocess = clip.load("ViT-B/32", device=device)
 
 raw_data = {
-    "task5": "a person walks and then makes a right turn",
-    "task7": "a man walks forward then stops",
+    "task5": "a person walks and then makes a right turn", # good
+    "task7": "a man walks forward then stops", # good
     "task14": "a person wonders in an oval path and ends where he started",
     "task19": "a man runs to the right then runs to the left then back to the middle",
-    "task20": "person is walking without their arms swinging",
+    "task20": "a person is walking forward briskly", # good
     "task21": "a person walks in a counterclockwise circle",
     "task23": "a person walks forward then turns around and walks back",
     "task26": "a person paces back and forth in a small space",
@@ -20,7 +20,7 @@ raw_data = {
     "task36": "person walks up and then spins counter clockwise then walks back",
     "task52": "a person walks to the right in a partial circle",
     "task59": "a person casually walks in a figure 8 pattern",
-    "task62": "a person shuffles from the left to the right, then shuffles back to the left",
+    "task62": "a person shuffles from the left to the right, then shuffles back to the left", # good
     "task79": "a person turns left, then turns right, then turns left again",
     "task89": "he is running straight and stopped",
     "task3": "a man lifts something on his left and places it down on his right",
@@ -29,15 +29,16 @@ raw_data = {
     "task16": "someone is playing the violin",
     "task29": "man fix his tie in the mirror",
     "task38": "a person bends their arms at the elbow and moves their left arm up and down",
-    # "task46": ,
-    # "task49": ,
-    # "task51": ,
-    # "task54": ,
-    # "task55": ,
-    # "task60": ,
-    # "task84": ,
-    # "task85": ,
-    # "task87": ,
+    "task46": "a person lifts their hand up to their head then lowers it", # not bad
+    "task49": "he uplift his right hand and moving it fast again and again", # good 
+    "task51": "a person raised both their arms and started to clap", # not bad
+    "task54": "he is waving with his right hand", # good
+    "task55": "a person is lifting weights",
+    "task60": "a person claps their hands together, stops, then starts again",
+    "task84": "a person shrugs their shoulders and swings their arms", # good
+    "task85": "a man strums an instrument with his right hand",
+    "task87": "a person standing brings hands together in front of him to applaud", # good
+    
     "task101": "a man walks backward, and he waves his right hand",
     "task102": "then he turns right and walks forward",
 }
